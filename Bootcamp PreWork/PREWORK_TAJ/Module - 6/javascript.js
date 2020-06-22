09a7252a634842fdac73b8ef4e18e5689fda61d8
sunshine <tajohnsonn@gmail.com>
2020-06-22 00:02:31 -0700

    initial commit

---
+++ Bootcamp PreWork/PREWORK_TAJ/Module - 6/javascript.js
@@ -0,0 +1,40 @@
+<script>
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px"; 
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.backgroundColor = "Orange";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.opacity = "100";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.5"; 
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+document.getElementById("box").style.backgroundColor = "Blue";
+
+});
+
+
+</script>
